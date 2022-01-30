6ac5ae6e26cf3b7e7901f171756c25b3123db4f0
Adithyan k P <adithyankp777@gmail.com>
2022-01-30 15:20:05 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { Header } from "./components/Header";
+import "./App.css";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container"></div>
+      <Balance />
+      <IncomeExpenses />
+      <TransactionList />
+    </div>
+  );
 }
 
 export default App;
